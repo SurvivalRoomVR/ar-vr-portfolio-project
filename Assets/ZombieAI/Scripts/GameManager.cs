4652508c266904c1b9fdd61cbe4fffdea6f79321
santiagopemo <santiagopemo@gmail.com>
2021-09-11 16:31:17 -0500

    added blody fonts

--- Assets/ZombieAI/Scripts/GameManager.cs
+++ Assets/ZombieAI/Scripts/GameManager.cs
@@ -8,21 +8,26 @@ public class GameManager : MonoBehaviour
     public Canvas loseCanvas;
     public GameObject player;
     public AudioMixer audioMixer;
+
+    private float startTime;
+    public float currentTime = 0;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         player.GetComponent<PlayerHealth>().OnZeroHealth += GameOver;
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        currentTime = Time.time - startTime;
     }
     public void GameOver()
     {
         loseCanvas.GetComponent<Animator>().CrossFadeInFixedTime("LoseCanvas", 0.1f);
         audioMixer.SetFloat("ZombiesVolume", -80.0f);
+        Debug.Log(currentTime);
     }
 }
